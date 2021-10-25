ff317fd5adfae7fd2d433fb3b28ee228f6717e12
hendriawanswatandika <hendriawanswatandika@localhost>
2021-10-25 10:43:27 +0700

    3rd

---
+++ Test Suites/Janus.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Janus</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0902cd01-12fe-4b2a-984a-f4fd593fd6a3</testSuiteGuid>
+   <testCaseLink>
+      <guid>74713996-2098-4897-ab60-fcc2a818c2ff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
